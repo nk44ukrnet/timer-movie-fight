a66c4c81e367b158cdad67113377dee73c8e840e
Віктор Ковтун <v.kovtun@ies.org.ua>
2020-11-02 13:57:45 +0200

    json added

--- js_prac/food/js/script.js
+++ js_prac/food/js/script.js
@@ -215,18 +215,25 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const menuField = document.querySelector('.menu__field .container');
     class MenuesClass {
-        constructor(img, title, desc, price, append) {
+        constructor(img, title, desc, price, append, transfer = 1, ...classes) {
             this.img = img;
             this.title = title;
             this.desc = desc;
             this.price = price;
             this.append = append;
+            this.transfer = transfer;
+            this.classes = classes;
+
+            this.changeToUAH();
             this.insertToThePage();
         }
-        insertToThePage(){
+        changeToUAH(){
+            this.price = this.price * this.transfer;
+        }
+        insertToThePage() {
             this.append.innerHTML += `
             <div class="menu__item">
-            <img src="${this.img}" alt="img">
+            <img src="${this.img}" alt="${this.title}">
             <h3 class="menu__item-subtitle">${this.title}</h3>
             <div class="menu__item-descr">${this.desc}</div>
             <div class="menu__item-divider"></div>
