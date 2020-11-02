64709851eaa4f2b23fe8aef48e64f51df9f645de
Віктор Ковтун <v.kovtun@ies.org.ua>
2020-11-02 12:49:02 +0200

    food update classes

--- js_prac/food/js/script.js
+++ js_prac/food/js/script.js
@@ -121,17 +121,142 @@ document.addEventListener('DOMContentLoaded', () => {
     document.addEventListener('keydown', e => {
         if (e.keyCode === 27 && modal.classList.contains('show')) {
             closeModal();
-    }
+        }
     });
 
-    const modalTimerId = setTimeout(openModal, 2000);
+    //const modalTimerId = setTimeout(openModal, 15000);
 
     function showModalByScroll() {
-        if((window.pageYOffset + document.documentElement.clientHeight) >= document.documentElement.scrollHeight) {
+        if ((window.pageYOffset + document.documentElement.clientHeight) >= document.documentElement.scrollHeight) {
             openModal();
             window.removeEventListener('scroll', showModalByScroll);
         }
     }
 
     window.addEventListener('scroll', showModalByScroll);
+
+    // Classes img, name, description, price
+    //version1
+    /*class GoodFromStore {
+        constructor(img, name, desc, price){
+            this.img = img;
+            this.name = name;
+            this.desc = desc;
+            this.price = price;
+        }
+    }
+    class GoodsFromStoreInsert extends GoodFromStore{
+        constructor(img, name, desc, price, insert1, insert2, insert3, insert4){
+            super(img, name, desc, price);
+            this.insert1 = insert1;
+            this.insert2 = insert2;
+            this.insert3 = insert3;
+            this.insert4 = insert4;
+        }
+        insertIntoPage(){
+            this.insert1.src = this.img;
+            this.insert2.innerHTML = `${this.name}`;
+            this.insert3.innerHTML = `${this.desc}`;
+            this.insert4.innerHTML = `${this.price}`;
+        }
+    }
+    // const insertData = new GoodsFromStoreInsert;
+
+    const menuItem = document.querySelectorAll('.menu__item');
+    menuItem.forEach((item, index) => {
+        let itemImg = item.querySelector('img'),
+            itemName = item.querySelector('.menu__item-subtitle'),
+            itemDesc = item.querySelector('.menu__item-descr'),
+            itemPrice = item.querySelector('.menu__item-total span');
+        let insertData;
+        switch(index) {
+            case 0:
+                 insertData = new GoodsFromStoreInsert(
+                    'img/tabs/vegy.jpg',
+                    'Меню "Фитнес1"',
+                    'Меню "Фитнес1" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+                    '229',
+                    itemImg,
+                    itemName,
+                    itemDesc,
+                    itemPrice);
+                insertData.insertIntoPage();
+                break;
+
+            case 1:
+                insertData = new GoodsFromStoreInsert(
+                    'img/tabs/elite.jpg',
+                    'Меню “Премиум2”',
+                    'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+                    '550',
+                    itemImg,
+                    itemName,
+                    itemDesc,
+                    itemPrice);
+                insertData.insertIntoPage();
+                break;
+
+            case 2:
+                 insertData = new GoodsFromStoreInsert(
+                    'img/tabs/post.jpg',
+                    'Меню "Постное3"',
+                    'Меню “Постное3” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+                    '430',
+                    itemImg,
+                    itemName,
+                    itemDesc,
+                    itemPrice);
+                insertData.insertIntoPage();
+                break;
+        }
+    })*/
+
+    //version2
+
+    const menuField = document.querySelector('.menu__field .container');
+    class MenuesClass {
+        constructor(img, title, desc, price, append) {
+            this.img = img;
+            this.title = title;
+            this.desc = desc;
+            this.price = price;
+            this.append = append;
+            this.insertToThePage();
+        }
+        insertToThePage(){
+            this.append.innerHTML += `
+            <div class="menu__item">
+            <img src="${this.img}" alt="img">
+            <h3 class="menu__item-subtitle">${this.title}</h3>
+            <div class="menu__item-descr">${this.desc}</div>
+            <div class="menu__item-divider"></div>
+            <div class="menu__item-price">
+            <div class="menu__item-cost">Ціна:</div>
+            <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+            </div>
+            </div>
+            `;
+        }
+    }
+    const good1 = new MenuesClass(
+        'img/tabs/vegy.jpg',
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        '229',
+        menuField
+        );
+    const good2 = new MenuesClass(
+        'img/tabs/elite.jpg',
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        '550',
+        menuField
+    );
+    const good3 = new MenuesClass(
+        'img/tabs/post.jpg',
+        'Меню "Постное"',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        '430',
+        menuField
+    );
 });
