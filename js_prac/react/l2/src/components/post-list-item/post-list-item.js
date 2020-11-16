de312f5b8ee6be602e98c586e8ae2405a745eeb7
Віктор Ковтун <v.kovtun@ies.org.ua>
2020-11-16 18:00:28 +0200

    react l2 update

--- js_prac/react/l2/src/components/post-list-item/post-list-item.js
+++ js_prac/react/l2/src/components/post-list-item/post-list-item.js
@@ -5,23 +5,37 @@ export default class PostListItem extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            important: false
+            important: false,
+            like: false
         };
+        this.onImportant = this.onImportant.bind(this);
+        this.onLike = this.onLike.bind(this);
     }
     onImportant(){
         this.setState(({important}) =>({
             important: !important
         }))
     }
+    onLike(){
+        this.setState(({like}) =>({
+            like: !like
+        }))
+    }
     render() {
-        const {label, important = false} = this.props;
+        const {label} = this.props;
+        const {important, like} = this.state;
         let classNames = 'app-list-item d-flex justify-content-between';
         if (important) {
             classNames += ' important';
         }
+        if (like) {
+            classNames += ' like';
+        }
         return (
             <div className={classNames}>
-            <span className="app-list-item-label">
+            <span className="app-list-item-label"
+            onClick={this.onLike}
+            >
                 {label}
             </span>
                 <div className="d-flex justify-content-center align-items-center">
